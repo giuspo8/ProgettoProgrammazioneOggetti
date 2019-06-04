fbafeafd17163df5dd8e9661affd6f2d307427a2
DavideVita <davide.vitaletti@gmail.com>
2019-06-04 12:27:09 +0200

    Partner

---
+++ src/Partner.java
@@ -0,0 +1,16 @@
+
+public class Partner extends Institution {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
